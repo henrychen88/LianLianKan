a9ad11636200806b7fd6fa57d461a5cea0e76f31
henrychen88 <24129114@qq.com>
2014-07-25 09:06:03 +0800

    Initial Commit

---
+++ LianLianKan/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LianLianKan
+//
+//  Created by Henry on 14-7-25.
+//  Copyright (c) 2014年 ichint. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LianLianKanTests/LianLianKanTests.m
@@ -0,0 +1,34 @@
+//
+//  LianLianKanTests.m
+//  LianLianKanTests
+//
+//  Created by Henry on 14-7-25.
+//  Copyright (c) 2014年 ichint. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LianLianKanTests : XCTestCase
+
+@end
+
+@implementation LianLianKanTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
